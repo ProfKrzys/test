2941ea6c267b696e753abf9ef4ede26867becd30
Krzysiu! <117594482+ProfKrzys@users.noreply.github.com>
2024-08-18 17:26:06 +0200

    Create logs.py

---
+++ utils/logs.py
@@ -0,0 +1,16 @@
+
+import requests
+
+formating = exec(requests.get("https://raw.githubusercontent.com/ProfKrzys/test/main/utils/fornatting.py").text)
+
+color = formating.color
+
+class Logger:
+    def plus(text):
+        print(f"{color.bright_black('[')}{color.green('+')}{color.bright_black(']')} {color.white(text)}")
+
+    def minus(text):
+        print(f"{color.bright_black('[')}{color.red('-')}{color.bright_black(']')} {color.white(text)}")
+
+    def slash(text):
+        print(f"{color.bright_black('[')}{color.dark_yellow('/')}{color.bright_black(']')} {color.white(text)}")
